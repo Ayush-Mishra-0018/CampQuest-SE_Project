dfb0b0c0a53a96dab5ee8296a8c5571d1258fb72
iHarshSinha <harsh.sinha@iiitb.ac.in>
2025-11-29 19:42:04 +0530

    common setup

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   testEnvironment: 'node',
   roots: ['<rootDir>/tests'],
-  verbose: false,
+  verbose: true,
   collectCoverage: false,
+  testTimeout: 30000,
+  testMatch: [
+    '**/tests/**/*.test.js'
+  ]
 };
