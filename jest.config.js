f5124f9e94be1f517884ae4155698c29152970de
Ayush-Mishra-0018 <Ayush.Mishra129@iiitb.ac.in>
2025-12-06 23:12:42 +0530

    Fix config

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,24 @@
 module.exports = {
-  testEnvironment: 'node',
   roots: ['<rootDir>/tests'],
   verbose: true,
   collectCoverage: false,
   testTimeout: 30000,
-  testMatch: [
-    '**/tests/**/*.test.js'
-  ]
+
+  projects: [
+    {
+      displayName: 'node',
+      testEnvironment: 'node',
+      testMatch: ['**/tests/**/*.test.js'],
+      testPathIgnorePatterns: [
+        '/tests/unit/public/',
+      ],
+    },
+    {
+      displayName: 'jsdom',
+      testEnvironment: 'jsdom',
+      testMatch: [
+        '**/tests/unit/public/**/*.test.js',
+      ],
+    },
+  ],
 };
